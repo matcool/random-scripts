438b25cb0b30c6494e21c0dff6c2ec18b3a59539
MatCorgi <mateus44faria@gmail.com>
2018-03-01 20:21:04 -0300

    one of the first things i did on python
    trashest code

---
+++ protein_shake.py
@@ -0,0 +1,77 @@
+import time
+protein = 0
+print("Welcome to my hunk kitchen. Today I’m going to show you how to make the perfect protein shake.")
+time.sleep(4)
+print("This is something I make for myself, just about everyday. So I figured, why not show you?")
+time.sleep(3)
+print("The first thing you are going to want to do is put in a scoop of protein,")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(3)
+print("then you are going to want to add about a cup of almond milk to the mix.")
+time.sleep(3)
+print("Then you are going to add some protein.")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(3)
+print("And then I usually throwing a whole banana then we are going to add some protein")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(3)
+print("Now what I’d like to do just for a little bit of taste is add some protein.")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(3)
+print("Then I usually add one half of protein,")
+protein += 0.5
+print("Protein = "+str(protein))
+time.sleep(3)
+print("now of course there wouldn’t really be a shake without some ice cubes.")
+time.sleep(3)
+print("So we are going to add some protein,")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(3)
+print("take some ice cubes dip them in protein, throw them in the mix")
+protein += 2
+print("Protein = "+str(protein))
+time.sleep(3)
+print("then add some protein")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(3)
+print("and then – and only then can you add some protein and blend.")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(6)
+print("Now while you guys weren’t looking I actually went back and added some protein.")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(4)
+print("Once everything’s all blended you can pour the contents out into a glass,")
+time.sleep(3)
+print("add some protein.")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(2)
+print("And now you can finally add some protein and enjoy your beverage.")
+protein += 1
+print("Protein = "+str(protein))
+time.sleep(3)
+print("...")
+time.sleep(2)
+print("..")
+time.sleep(1)
+print(".")
+time.sleep(2)
+print("I forgot the protein.")
+protein += 1
+time.sleep(3)
+print("Total amount of protein = "+str(protein))
+protein *= 5
+print("Total grams = "+str(protein))
+protein *= 4
+print("Total kcal = "+str(protein))
+time.sleep(50)
+print("happy april fools (late)")
+time.sleep(10)
