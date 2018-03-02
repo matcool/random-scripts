b970c4d8567560f34a161fbfdfb2e12851d181b6
MatCorgi <mateus44faria@gmail.com>
2018-03-01 21:28:51 -0300

    a fuck i just found drawille
    this is useless now

--- imgtobraille.py
+++ imgtobraille.py
@@ -38,7 +38,7 @@ for y in range(0,h,3):
         t1 = ''
         ind = 1
         for i in checks:
-            if not grid[i[1]+y][i[0]+x]: t1+=str(ind)
+            if not grid[i[1]+y][i[0]+x]: t1+=str(ind) # remove not if u want reverse thing blablaa
             ind += 1
         if t1: t2 = eval(r'"\N{Braille Pattern Dots-%s}"' % t1)
         else: t2 = chr(10240) #eval(r'"\N{Braille Pattern Dots-%s}"' % '3')
