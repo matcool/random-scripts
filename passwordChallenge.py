922968a7828f537ed02ba9b360e3647973e0d623
MatCorgi <mateus44faria@gmail.com>
2018-07-07 15:42:40 -0300

    Create passwordChallenge.py
    super easy might do another one

---
+++ passwordChallenge.py
@@ -0,0 +1,13 @@
+import re as rm;import base64 as m432
+g=input("password: ")
+def compriment(p): return sum([len({}),max([-1*0,len("")+4-5+1])])+len(p)
+def wrong(): global Wrong; Wrong=True
+s="4b44383657324574656b4574576c31374e4831664b5";Wrong=False
+s=s+"87379665674684c5731424c5530774c546c64657a5239"
+for i in range(0,len(s),2): results= (ord(chr(int(s[i:i+2],16))))+len(s)+(int("FF",16)+int(str(2)))
+if compriment(g)!=compriment(s)/8+4-1: wrong()
+if rm.match(str(m432.b64decode("".join([chr(int(s[i:i+2],int(results/100)*4)) for i in range(0,len(s),2)])))[2:-1],g)==None: wrong()
+for i in range(3,len(s)+1,3): big = (int((ord(chr(44))+i)*3)/3)*int("3F",16)/4
+sums = [sum([ord(i) for i in j]) for j in g.split("_")]
+if Wrong or sums!=[int(big/100)*20+(pow(6,2))-5,int(str(big/results)[3]+str(big/results)[13])*10+(int(str(big/results)[16])-1), int(results+(big/1000))/2+9**2-1]: print("WRONG")
+else: print("CONGRATS!")
